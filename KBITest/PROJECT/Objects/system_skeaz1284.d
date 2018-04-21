c9f5f89259d79ae3185d635b602cb81459b710db
YTomTJ <ybb331082@126.com>
2018-04-22 02:03:40 +0800

    update for I modified key event 4

--- KBITest/PROJECT/Objects/system_skeaz1284.d
+++
@@ -1,9 +0,0 @@
-.\objects\system_skeaz1284.o: ..\CORE\system_SKEAZ1284.c
-.\objects\system_skeaz1284.o: D:\Program Files\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-.\objects\system_skeaz1284.o: ..\CORE\SKEAZ1284.h
-.\objects\system_skeaz1284.o: ..\CORE\core_cm0plus.h
-.\objects\system_skeaz1284.o: ..\CORE\core_cmInstr.h
-.\objects\system_skeaz1284.o: ..\CORE\SKEAZ1284.h
-.\objects\system_skeaz1284.o: ..\CORE\core_cmFunc.h
-.\objects\system_skeaz1284.o: ..\CORE\SKEAZ1284.h
-.\objects\system_skeaz1284.o: ..\CORE\system_SKEAZ1284.h
